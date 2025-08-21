5d661574730a771685d132f5c84125d01c36fdf6
Esli Rodrigues <eslicdm1@gmail.com>
2025-08-21 18:20:33 -0300

    Initial commit

---
+++ src/main/java/com/eslirodrigues/service_app_java/ServiceAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.service_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eslirodrigues/service_app_java/ServiceAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.service_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
